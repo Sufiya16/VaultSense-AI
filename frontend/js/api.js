9c2be06c8f23f02480da4ea209f665ae03a69c22
Sufiya16 <sufiyam1606@gmail.com>
2026-05-19 00:11:55 +0530

    Connect frontend to deployed backend

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://127.0.0.1:8000";
+const API_BASE = "https://vaultsense-api.onrender.com";
 
 async function assessRisk(payload) {
     const response = await fetch(`${API_BASE}/assess`, {
